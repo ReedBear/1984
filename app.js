dee451049de1ad35fb209f062ce43fe4ea4692d4
Julien Seren-Rosso <j.serenrosso@gmail.com>
2011-09-27 15:28:32 +0200

    Mouvement de la souris, temps réel
    Gestion du mouvement de la souris / connexion / déconnexion.

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = require('express').createServer(), 
+	express = require('express'),
+	io = require('socket.io').listen(app);
+
+app.use(express.static(__dirname + '/client'));
+app.listen(80);
+
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/client/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+	var id = socket.id;
+	
+	// Réponse
+	socket.emit('connected', id);
+	
+	// Update de la position
+	socket.on('updatePosition', function (data) {
+		// Broadcast de la position
+		socket.broadcast.volatile.emit('pushPosition', data);
+	});
+	
+	// Déconnexion
+	socket.on('disconnect', function () {
+		socket.broadcast.emit('disconnected', id);
+	});
+});
